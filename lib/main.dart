c8891a38daa3b14ad9684bf9295fb480795acec3
cmwema <calebmwema65@gmail.com>
2022-12-28 20:21:56 +0300

    Created the app skeleton

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.teal,
+          title: const Center(
+            child: Text('Micard'),
+          ),
+        ),
+        backgroundColor: Colors.white,
+      ),
+    );
+  }
+}
