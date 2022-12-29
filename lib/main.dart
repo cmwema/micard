f4e6165555814a055ea19be74d3cd93a0426dcad
cmwema <calebmwema65@gmail.com>
2022-12-29 10:11:48 +0300

    Challenge complete

--- lib/main.dart
+++ lib/main.dart
@@ -20,13 +20,48 @@ class MyApp extends StatelessWidget {
           ),
         ),
         body: SafeArea(
-          child: Container(
-            color: Colors.teal[50],
-            height: 100.0,
-            width: 100.0,
-            margin: const EdgeInsets.all(20.0),
-            padding: const EdgeInsets.all(20.0),
-            child: const Text("Hello"),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.all(20.0),
+                margin: const EdgeInsets.all(10.0),
+                color: Colors.blueGrey[200],
+                width: 100.0,
+                child: const Text("Container 1"),
+              ),
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      height: 100.0,
+                      width: 100.0,
+                      color: Colors.teal[200],
+                      margin: const EdgeInsets.all(10.0),
+                      padding: const EdgeInsets.all(20.0),
+                      child: const Text("Center 1"),
+                    ),
+                    Container(
+                      height: 100.0,
+                      width: 100.0,
+                      color: Colors.red[500],
+                      margin: EdgeInsets.all(10.0),
+                      padding: EdgeInsets.all(20.0),
+                      child: const Text("Center 2"),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(20.0),
+                margin: const EdgeInsets.all(10.0),
+                color: Colors.teal[200],
+                width: 100.0,
+                child: const Text("Container 1"),
+              ),
+            ],
           ),
         ),
         backgroundColor: Colors.white,
